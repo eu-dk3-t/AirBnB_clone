8ffbbf48b46e3d318196e9acbd2d0a64e217efff
eu-dk3-t <ze_3fanuel@proton.me>
2023-10-12 22:11:53 +0000

    __init__ file/method for models dir

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""__init__ method for models"""
+from models.engine.file_storage import FileStorage
+
+
+# Initialize 'FileStorage` instance
+# and loads JSON file into a dict
+storage = FileStorage()
+storage.reload()
