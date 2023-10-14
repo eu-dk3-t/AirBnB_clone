59e3e7bc280f840ee5e73f7e77b17426b1346797
Shakvilla <abdulshakuraclement@yahoo.com>
2023-10-14 17:03:22 +0000

    implemented the quite command prompt

--- console.py
+++ console.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+"""
+    Implementation of the console for the alx AirBnB project
+"""
+import cmd
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+class HBNBCommand(cmd.Cmd):
+    """
+        The entry point of the command interpreter
+    """
+
+    prompt = ("(hbnb)")
+
+    def do_quit(self, args):
+        """
+            Command to quite and exit the program
+        """
+        return True
